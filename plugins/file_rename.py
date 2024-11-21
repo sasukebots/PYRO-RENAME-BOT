168b856969cf85698df761d8e49ee91cfe239164
sasukebots <mrsasuke823@gmail.com>
2024-11-21 19:40:51 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -18,8 +18,8 @@ import os, time
 async def rename_handler(client, message):
     file = getattr(message, message.media.value)
     filename = file.file_name  
-    if file.file_size > 2000 * 1024 * 1024:
-        return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ")
+    if file.file_size > 4000 * 1024 * 1024 * 1024 * 1024:
+        return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 4Gʙ")
 
     try:
         await message.reply_text(
